1cde3bb6265a0332fc0cb731d43951d0901de0d0
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-11-14 18:51:27 +0100

    fix:black del settgins que se me olvido

--- backend/diaflow/settings.py
+++ backend/diaflow/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "comidas",
     "kits",
     "reportes",
-
     "corsheaders",
 ]
 
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-
     "corsheaders.middleware.CorsMiddleware",
 ]
 
