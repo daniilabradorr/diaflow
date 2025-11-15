95c0cf2d71ce57ad759a5668cea727cd6f662775
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-11-15 10:02:05 +0100

    fix: arregalndo algunos eereos y dejarndolo listo para conectar el fetondend entero

--- backend/pacientes/apps.py
+++ backend/pacientes/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 import importlib
 
+
 class PacientesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pacientes"
--- backend/pacientes/signals.py
+++ backend/pacientes/signals.py
@@ -3,8 +3,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from pacientes.models import Paciente
 
+
 @receiver(post_save, sender=User)
 def crear_paciente_automatico(sender, instance, created, **kwargs):
     if created:
-        #al crear un nuevo usuario, creo un Paciente vinculado
+        # al crear un nuevo usuario, creo un Paciente vinculado
         Paciente.objects.create(usuario=instance, nombre=instance.username)
