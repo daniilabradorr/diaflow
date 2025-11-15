a7b5b954f527aaddb38d11c631b7562145cd07d7
Daniel Labrador Benito <danielabrador47@gmail.com>
2025-11-15 09:59:42 +0100

    fix:ALGUNOS ERRORES, preparadno para cinectar todo bien

--- backend/pacientes/apps.py
+++ backend/pacientes/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
-
+import importlib
 
 class PacientesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "pacientes"
+
+    def ready(self):
+        importlib.import_module("pacientes.signals")
---
+++ backend/pacientes/signals.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from pacientes.models import Paciente
+
+@receiver(post_save, sender=User)
+def crear_paciente_automatico(sender, instance, created, **kwargs):
+    if created:
+        #al crear un nuevo usuario, creo un Paciente vinculado
+        Paciente.objects.create(usuario=instance, nombre=instance.username)
